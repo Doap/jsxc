f2b3e2dce4238844b4d80ecb6d5999e5911ff03d
sualko <klaus@jsxc.org>
2021-01-20 13:29:52 +0100

    chore: enable no extra semi

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,7 +72,7 @@ module.exports = {
         "@typescript-eslint/no-empty-interface": "error",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-extra-non-null-assertion": "error",
-        "@typescript-eslint/no-extra-semi": "off",
+        "@typescript-eslint/no-extra-semi": "warn",
         "@typescript-eslint/no-floating-promises": "off",
         "@typescript-eslint/no-for-in-array": "error",
         "@typescript-eslint/no-implied-eval": "error",
